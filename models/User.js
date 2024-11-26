5567ed3a49c954b857c9ea935df0dfa57739d678
umidkhan <umidxondeveloper@gmail.com>
2024-11-26 22:25:19 +0500

    chatId type changed

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     chatId: {
-      type: String,
+      type: Number,
       required: true,
       uniqe: true,
     },
